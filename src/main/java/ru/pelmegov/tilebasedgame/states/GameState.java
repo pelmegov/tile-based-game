4852e5b0486e9078e48689dcc96015eed908b8d2
modkomi <modkomi@mail.ru>
2016-06-11 21:17:31 +0300

    Add maps

--- src/main/java/ru/pelmegov/tilebasedgame/states/GameState.java
+++ src/main/java/ru/pelmegov/tilebasedgame/states/GameState.java
@@ -3,6 +3,7 @@ package ru.pelmegov.tilebasedgame.states;
 import ru.pelmegov.tilebasedgame.Game;
 import ru.pelmegov.tilebasedgame.entities.creatures.Player;
 import ru.pelmegov.tilebasedgame.gfx.Assets;
+import ru.pelmegov.tilebasedgame.worlds.World;
 
 import java.awt.*;
 
@@ -12,21 +13,24 @@ import java.awt.*;
  */
 public class GameState extends State {
 
-    int x = 0;
-    Player player;
+    private Player player;
+    private World world;
 
     public GameState(Game game) {
         super(game);
         player = new Player(game, 100, 100);
+        world = new World("resources/worlds/world1.txt");
     }
 
     @Override
     public void tick() {
+        world.tick();
         player.tick();
     }
 
     @Override
     public void render(Graphics g) {
+        world.render(g);
         player.render(g);
     }
 }
